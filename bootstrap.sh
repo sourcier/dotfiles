c5e1824b4611f0f2b778a5e1ce4c29cdc7248ad2
Roger Rajaratnam <roger.rajaratnam@ft.com>
2017-11-21 17:25:46 +0000

    Add .gitignore and bootstrap.sh which setups dotfiles

---
+++ bootstrap.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+#-----------------------------------------------------------------------------
+# Functions
+#-----------------------------------------------------------------------------
+
+# Notice title
+notice() { echo -e "\033[1;32m=> $1\033[0m"; }
+
+# Error title
+error() { echo -e "\033[1;31m=> Error: $1\033[0m"; }
+
+# List item
+c_list() { echo -e "  \033[1;32m✔\033[0m  $1"; }
+
+# Error list item
+e_list() { echo -e "  \033[1;31m✖\033[0m  $1"; }
+
+# Check for dependency
+dep() {
+  type -p $1 &> /dev/null
+  local installed=$?
+  if [ $installed -eq 0 ]; then
+    c_list $1
+  else
+    e_list $1
+  fi
+  return $installed
+}
+
+install() {
+  local files=( $(ls -a) )
+  for file in "${files[@]}"; do
+    in_array $file "${excluded[@]}"
+    should_install=$?
+    if [ $should_install -gt 0 ]; then
+      [ -d "$HOME/$file" ] && rm -rf "$HOME/$file"
+      cp -Rf "$file" "$HOME/$file"
+    fi
+  done
+}
+
+in_array() {
+  local hay needle=$1
+  shift
+  for hay; do
+    [[ $hay == $needle ]] && return 0
+  done
+  return 1
+}
+
+#-----------------------------------------------------------------------------
+# Initialize
+#-----------------------------------------------------------------------------
+
+dotfiles_root=$PWD
+dependencies=(git)
+excluded=(. .. .git .gitignore bootstrap.sh README.md .DS_Store .idea)
+
+
+#-----------------------------------------------------------------------------
+# Dependencies
+#-----------------------------------------------------------------------------
+
+notice "Checking dependencies"
+
+not_met=0
+for need in "${dependencies[@]}"; do
+  dep $need
+  met=$?
+  not_met=$(echo "$not_met + $met" | bc)
+done
+
+if [ $not_met -gt 0 ]; then
+  error "$not_met dependencies not met!"
+  exit 1
+fi
+
+#-----------------------------------------------------------------------------
+# Install
+#-----------------------------------------------------------------------------
+
+# Assumes $dotfiles_root is *ours*
+if [ -d $dotfiles_root ]; then
+  pushd $dotfiles_root
+
+  # Update Repo
+  notice "Updating"
+  git pull origin master
+
+  # Install
+  notice "Installing"
+  install
+else
+  # Clone Repo
+  notice "Downloading"
+  git clone --recursive git@github.com:sourcier/dotfiles.git $dotfiles_root
+
+  pushd $dotfiles_root
+
+  # Backup
+  notice "Backup up old files ($backupdir)"
+  backup
+
+  # Install
+  notice "Installing"
+  install
+fi
+
+#-----------------------------------------------------------------------------
+# Finished
+#-----------------------------------------------------------------------------
+
+popd
+notice "Done"
+
+#source "$HOME/.zshrc"
