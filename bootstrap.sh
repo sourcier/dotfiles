9481e5d18e5d5afef27841e308edab9807ca4059
Roger Rajaratnam <roger@sourcier.uk>
2018-01-08 11:38:06 +0000

    Add vscode settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -55,7 +55,7 @@ in_array() {
 
 dotfiles_root=$PWD
 dependencies=(git)
-excluded=(. .. .git .gitignore bootstrap.sh README.md .DS_Store .idea)
+excluded=(. .. .git .gitignore bootstrap.sh README.md .DS_Store .idea vscode-settings.json)
 
 
 #-----------------------------------------------------------------------------
