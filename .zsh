f687101c0dcf7b252eace93d42ba63f32a2c9859
Roger Rajaratnam <roger@sourcier.uk>
2018-01-23 12:50:35 +0000

    Update vcs hash length to 7

--- .zsh
+++ .zsh
@@ -3,6 +3,7 @@ export ZSH=/Users/rrajaratnam/.oh-my-zsh
 # Theme settings
 POWERLEVEL9K_MODE="nerdfont-complete"
 POWERLEVEL9K_SHOW_CHANGESET=true
+POWERLEVEL9K_CHANGESET_HASH_LENGTH=7
 POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history)
