5d8a86bcd199e131402307a3713bc505084a8c29
Roger Rajaratnam <roger@sourcier.uk>
2018-10-16 21:18:15 +0100

    Update zsh settings

--- .zsh
+++ .zsh
@@ -1,4 +1,4 @@
-export ZSH=/Users/rrajaratnam/.oh-my-zsh
+export ZSH=/Users/roger/.oh-my-zsh
 
 # History settings
 HISTSIZE=1024
@@ -7,14 +7,7 @@ HISTORY_IGNORE="(ls|cd|pwd|exit|date|* --help)"
 setopt HIST_IGNORE_ALL_DUPS
 
 # Theme settings
-POWERLEVEL9K_MODE="nerdfont-complete"
-POWERLEVEL9K_SHOW_CHANGESET=true
-POWERLEVEL9K_CHANGESET_HASH_LENGTH=7
-POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history)
-DEFAULT_USER=$USER
-ZSH_THEME="powerlevel9k/powerlevel9k"
+ZSH_THEME="robbyrussell"
 
 # Plugins
 plugins=(git)
