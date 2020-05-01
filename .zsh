21d0bc95dcfb5430b299fdd806c420bfeb500af1
Roger Rajaratnam <roger@sourcier.uk>
2020-05-01 12:04:29 +0100

    Update history settings

--- .zsh
+++ .zsh
@@ -1,12 +1,5 @@
 export ZSH="/Users/$(whoami)/.oh-my-zsh"
 
-# History settings
-HISTSIZE=1024
-SAVEHIST=$HISTSIZE
-HISTORY_IGNORE="(ls|cd|pwd|exit|date|clear|* --help)"
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_IGNORE_SPACE
-
 # Theme settings
 ZSH_THEME=powerlevel10k/powerlevel10k
 
